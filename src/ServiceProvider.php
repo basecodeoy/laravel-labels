c0a00a1126432cffa04407f9bd99e5a89c5ae074
Brian Faust <hello@basecode.sh>
2023-03-12 06:00:38 +0200

    ci: update configurations

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -4,22 +4,16 @@ declare(strict_types=1);
 
 namespace PreemStudio\Labels;
 
-use Spatie\LaravelPackageTools\Commands\InstallCommand;
-use Spatie\LaravelPackageTools\Package;
-use Spatie\LaravelPackageTools\PackageServiceProvider;
+use PreemStudio\Jetpack\Package\AbstractServiceProvider;
+use PreemStudio\Jetpack\Package\Package;
 
-class ServiceProvider extends PackageServiceProvider
+class ServiceProvider extends AbstractServiceProvider
 {
     public function configurePackage(Package $package): void
     {
         $package
             ->name('laravel-labels')
             ->hasConfigFile('laravel-labels')
-            ->hasMigration('create_labels_table')
-            ->hasInstallCommand(function (InstallCommand $command) {
-                $command
-                    ->publishConfigFile()
-                    ->publishMigrations();
-            });
+            ->hasMigration('create_labels_table');
     }
 }
