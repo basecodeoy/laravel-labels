6fe25180eb9a38f67037146753754f620c54dfb7
Brian Faust <brian@faust.de.com>
2024-11-05 07:38:30 +0200

    first commit
    Signed-off-by: Brian Faust <brian@faust.de.com>

---
+++ tests/Unit/Concerns/HasLabelsTest.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Concerns;
+
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Collection;
+use Tests\Fixtures\ClassThatHasLabels;
+
+uses(RefreshDatabase::class);
+
+it('should morph to a labelable', function (): void {
+    $user = ClassThatHasLabels::create([
+        'name' => $this->faker->name,
+        'email' => $this->faker->safeEmail,
+        'password' => $this->faker->password,
+    ]);
+
+    $user->labels()->create([
+        'name' => $this->faker->firstName,
+        'description' => $this->faker->paragraph,
+        'color' => $this->faker->hexColor,
+    ]);
+
+    expect($user->labels())->toBeInstanceOf(MorphMany::class);
+    expect($user->labels)->toBeInstanceOf(Collection::class);
+});
---
+++ tests/Unit/Models/LabelTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Models;
+
+use BaseCodeOy\Labels\Models\Label;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\Fixtures\ClassThatHasLabels;
+
+uses(RefreshDatabase::class);
+
+it('can be found by its slug', function (): void {
+    Model::unguard();
+
+    $user = ClassThatHasLabels::create([
+        'name' => $this->faker->name,
+        'email' => $this->faker->safeEmail,
+        'password' => $this->faker->password,
+    ]);
+
+    $label = Label::create([
+        'labelable_id' => $user->id,
+        'labelable_type' => ClassThatHasLabels::class,
+        'name' => $this->faker->firstName,
+        'description' => $this->faker->paragraph,
+        'color' => $this->faker->hexColor,
+    ]);
+
+    expect($label->id)->toBe(Label::findBySlug($label->slug)->id);
+});
